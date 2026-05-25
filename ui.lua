cf09abb2ec5ce24723527ad2570a3bc569f787cc
gioplaysthis-ctrl <gioplaysthis@gmail.com>
2026-05-25 14:54:30 +0400

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,59 +1,16 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
 local Library = {}
 
 function Library:CreateWindow(info)
-    local Window = {
-        Tabs = {}
-    }
-
-    function Window:Set(bool)
-        print("Window Set:", bool)
-    end
-
-    function Window:Flush()
-        print("Window flushed")
-    end
-
-    function Window:CreateTab(tabinfo)
-        local Tab = {}
-
-        function Tab:CreateSection(info) end
-        function Tab:CreateDivider() end
-
-        function Tab:CreateToggle(info)
-            return {
-                Get = function() return info.Value end,
-                Set = function() end
-            }
-        end
-
-        function Tab:CreateSlider(info)
-            return {
-                Get = function() return info.Value end,
-                Set = function() end
-            }
-        end
-
-        function Tab:CreateInput(info)
-            return {
-                Get = function() return info.Value end,
-                Set = function() end
-            }
-        end
-
-        function Tab:CreateButton(info)
-            return {}
-        end
-
-        function Tab:CreatePicker(info)
-            return {
-                Get = function() return info.Value end,
-                Set = function() end
-            }
-        end
-
-        Window.Tabs[tabinfo.Name] = Tab
-        return Tab
-    end
+    local Window = Rayfield:CreateWindow({
+        Name = info.Name or "Window",
+        LoadingTitle = "Loading",
+        LoadingSubtitle = "by Biggie Hub",
+        ConfigurationSaving = {
+            Enabled = false
+        }
+    })
 
     return Window
 end
